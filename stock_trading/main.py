3c7baa9b10fda37e48484da3fc36b4c45955ec0b
mcapuccini <marco.capuccini@farmbio.uu.se>
2020-04-16 14:25:06 +0000

    DQN test

---
+++ stock_trading/main.py
@@ -0,0 +1,21 @@
+import csv
+from urllib.request import urlopen
+
+import pandas as pd
+
+from env import MultiStockEnv
+from stable_baselines.common.env_checker import check_env
+from stable_baselines import DQN
+
+# Get stocks
+STOCKS_URL = 'https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/aapl_msi_sbux.csv'
+response = urlopen(STOCKS_URL)
+data = pd.read_csv(response).values
+
+# Init and check the environment
+env = MultiStockEnv(data)
+check_env(env)
+
+# Train
+model = DQN('MlpPolicy', env, verbose=1, tensorboard_log="./dqn_stocks_tensorboard/")
+model.learn(total_timesteps=5000)
