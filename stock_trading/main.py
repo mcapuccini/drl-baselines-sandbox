f8f665292bca790f1ac7062da799c3ac298344ca
mcapuccini <marco.capuccini@farmbio.uu.se>
2020-04-21 14:58:38 +0000

    add arguments

--- stock_trading/main.py
+++ stock_trading/main.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 from urllib.request import urlopen
 
@@ -7,23 +8,29 @@ from stable_baselines.common.env_checker import check_env
 
 from env import MultiStockEnv
 
-# Get stocks
-STOCKS_URL = 'https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/aapl_msi_sbux.csv'
-response = urlopen(STOCKS_URL)
-data = pd.read_csv(response).values
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logdir', default='./logs', type=str)
+    parser.add_argument('--timesteps', default=100000, type=int)
+    args = parser.parse_args()
 
-# Init and check the environment
-env = MultiStockEnv(data, initial_investment=20000)
-check_env(env)
+    # Get stocks
+    STOCKS_URL = 'https://raw.githubusercontent.com/lazyprogrammer/machine_learning_examples/master/tf2.0/aapl_msi_sbux.csv'
+    response = urlopen(STOCKS_URL)
+    data = pd.read_csv(response).values
 
-# Train
-model = DQN('MlpPolicy', env, verbose=1, tensorboard_log="./logs",
-            # hyperparameters
-            gamma=0.95,
-            buffer_size=500,
-            exploration_final_eps=0.01,
-            double_q=False,
-            learning_starts=500,
-            target_network_update_freq=30)
+    # Init and check the environment
+    env = MultiStockEnv(data, initial_investment=20000)
+    check_env(env)
 
-model.learn(total_timesteps=100000, tb_log_name="stock_trading")
+    # Train
+    model = DQN('MlpPolicy', env, verbose=1, tensorboard_log=args.logdir,
+                # hyperparameters
+                gamma=0.95,
+                buffer_size=500,
+                exploration_final_eps=0.01,
+                double_q=False,
+                learning_starts=500,
+                target_network_update_freq=30)
+    model.learn(total_timesteps=args.timesteps, tb_log_name='stock_trading')
